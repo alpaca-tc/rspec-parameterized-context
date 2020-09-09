5ea7207cbf2c5edd6ff4c3ba9b5ce09c0d7d799e
alpaca-tc <alpaca-tc@alpaca.tc>
2020-09-10 02:13:33 +0900

    Support let/context

--- spec/rspec_parameterized_context_spec.rb
+++ spec/rspec_parameterized_context_spec.rb
@@ -3,15 +3,47 @@ RSpec.describe RspecParameterizedContext do
     describe '#parameterized' do
       extend RSpecParameterizedContext
 
-      parameterized do
-        where(:a, :b, :expected, size: 1) do
-          [
-            [1, 2, 3]
-          ]
-        end
+      context 'basic' do
+        parameterized do
+          where(:a, :b, :expected, size: 1) do
+            [
+              [1, 2, 3]
+            ]
+          end
 
-        with_them do
-          it { expect(a + b).to eq(expected) }
+          with_them do
+            it { expect(a + b).to eq(expected) }
+          end
+        end
+      end
+
+      context 'subject/let/context' do
+        parameterized do
+          subject do
+            expected
+          end
+
+          let(:a) do
+            'a'
+          end
+
+          where(:expected, size: 1) do
+            [
+              [a]
+            ]
+          end
+
+          with_them do
+            it { expect(expected).to eq(expected) }
+
+            context 'with context' do
+              let(:a) do
+                'b'
+              end
+
+              it { is_expected.to eq(a) }
+            end
+          end
         end
       end
     end
