f98ed2fcac6c8e5ca15fcc2c796da54340584629
alpaca-tc <alpaca-tc@alpaca.tc>
2020-09-09 14:55:01 +0900

    Initialize repository

---
+++ lib/rspec/parameterized/context.rb
@@ -0,0 +1,10 @@
+require "rspec/parameterized/context/version"
+
+module Rspec
+  module Parameterized
+    module Context
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rspec/parameterized/context/version.rb
@@ -0,0 +1,7 @@
+module Rspec
+  module Parameterized
+    module Context
+      VERSION = "0.1.0"
+    end
+  end
+end
