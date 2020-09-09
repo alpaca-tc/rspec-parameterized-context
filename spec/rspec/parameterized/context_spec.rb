d6f9e0cdf96daaad2a5db3592e9f8a9981d031c6
alpaca-tc <alpaca-tc@alpaca.tc>
2020-09-09 15:00:35 +0900

    Remove unused file

--- spec/rspec/parameterized/context_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Rspec::Parameterized::Context do
-  it "has a version number" do
-    expect(Rspec::Parameterized::Context::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
